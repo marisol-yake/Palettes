04e24a6d2dfd31b8e85dc05379efa7d0f0779faa
Marikit Yake <lilycl@protonmail.com>
2022-05-20 20:52:39 -0600

    update Palettes..jl

--- src/Palettes.jl
+++ src/Palettes.jl
@@ -4,13 +4,13 @@ using Colors
 using ColorTypes
 using Random
 
-include("./convertColorCodes.jl")
-include("./colorFamilies.jl")
-include("./colorPicker.jl")
-
 export HSV_to_RGB, hex_to_RGB, RGB_to_HSV
 export HSV_to_hex , hex_to_HSV
 export pastel_s30, pastel_s50
 export random_HSV, random_hue, random_saturation, random_value
+
+include("./convertColorCodes.jl")
+include("./colorFamilies.jl")
+include("./colorPicker.jl")
 
 end
